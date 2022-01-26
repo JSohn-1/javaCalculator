a7a46175452e26c1586d928c9207ba83be6d65b5
ProEpicGamer1 <40511868+ProEpicGamer1@users.noreply.github.com>
2022-01-26 08:35:21 -0800

    Update and rename calRunner.java to calcRunner.java

---
+++ calcRunner.java
@@ -0,0 +1,8 @@
+public class calcRunner{
+    public static void main(String[] args){
+        System.out.println(mathFunc.isPrime(10));
+        
+        System.out.println(mathFunc.simplifyRad(200));
+    }
+}
+
