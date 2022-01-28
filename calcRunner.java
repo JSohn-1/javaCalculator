2d915b603255d71020f7a8e96fe52dc88dad4536
ProEpicGamer1 <40511868+ProEpicGamer1@users.noreply.github.com>
2022-01-27 23:34:27 -0800

    Update calcRunner.java

--- calcRunner.java
+++ calcRunner.java
@@ -1,8 +1,14 @@
-public class calcRunner{
-    public static void main(String[] args){
+import java.util.Arrays;
+
+public class calcRunner {
+    public static void main(String[] args) {
         System.out.println(mathFunc.isPrime(10));
-        
-        System.out.println(mathFunc.simplifyRad(200));
+
+        System.out.println(Arrays.toString(mathFunc.simplifyRad(200)));
+
+        System.out.println(mathFunc.twoPoints(1, 1, 2, 2));
+
+        System.out.println(mathFunc.onePoint(1, 1, 3));
+
     }
 }
-
