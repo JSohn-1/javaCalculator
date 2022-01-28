b897c42dfa051bc7cdcf437aa8a457c1521ef586
ProEpicGamer1 <40511868+ProEpicGamer1@users.noreply.github.com>
2022-01-27 23:33:33 -0800

    Update mathFunc.java

--- mathFunc.java
+++ mathFunc.java
@@ -1,54 +1,54 @@
 import java.lang.Math;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.HashMap
-import java.util.Map
+import java.util.HashMap;
+import java.util.Map;
 
-public class mathFunc{
-    
-    //Basic Methods
-    public static boolean isPrime(int n){
+public class mathFunc {
+
+    // Basic Methods
+    public static boolean isPrime(int n) {
         int flag = 0;
-          
-        if(n > 1){
-            for(int i = 2; i < (int) Math.sqrt(n) + 1; i++){
-                if(n % i == 0){
+
+        if (n > 1) {
+            for (int i = 2; i < (int) Math.sqrt(n) + 1; i++) {
+                if (n % i == 0) {
                     flag = 1;
                     break;
                 }
             }
-            
-            if(flag == 0){
+
+            if (flag == 0) {
                 return true;
-            }else{
+            } else {
                 return false;
             }
-        }else{
+        } else {
             return false;
         }
     }
-    
-    //Advanced Methods
-    
-    public static int[] simplifyRad(int n){
+
+    // Advanced Methods
+
+    public static int[] simplifyRad(int n) {
         ArrayList<ArrayList<Integer>> numbers = new ArrayList<ArrayList<Integer>>();
         int[] fNumbers = new int[2];
-        
+
         numbers.add(new ArrayList<Integer>());
         numbers.add(new ArrayList<Integer>());
         fNumbers[0] = 1;
         fNumbers[1] = 1;
-        
+
         int total = 1;
         int totalS = 1;
-        
-        while(true){
-            if(isPrime(n)){
+
+        while (true) {
+            if (isPrime(n)) {
                 numbers.get(0).add(n);
                 break;
-            }else{
-                for(int i = 1; i < n; i++){
-                    if(isPrime(i) && (n % i == 0)){
+            } else {
+                for (int i = 1; i < n; i++) {
+                    if (isPrime(i) && (n % i == 0)) {
                         numbers.get(0).add(i);
                         n /= i;
                         break;
@@ -56,33 +56,66 @@ public class mathFunc{
                 }
             }
         }
-        
-        try{
-            while(true){
-                for(int i = 0; i < numbers.get(0).size(); i++){
-                    if(numbers.get(0).get(i) == numbers.get(0).get(i+1)){
+
+        try {
+            while (true) {
+                for (int i = 0; i < numbers.get(0).size(); i++) {
+                    if (numbers.get(0).get(i) == numbers.get(0).get(i + 1)) {
                         numbers.get(1).add(numbers.get(0).get(i));
                         numbers.get(0).remove(i);
                         numbers.get(0).remove(i);
-                        
+
                         break;
                     }
                 }
             }
-        }finally{
-            for(int i = 0; i < numbers.get(0).size(); i++){
-                fNumbers[0] = fNumbers[0] *numbers.get(0).get(i);
+        } finally {
+            for (int i = 0; i < numbers.get(0).size(); i++) {
+                fNumbers[0] = fNumbers[0] * numbers.get(0).get(i);
             }
-            for(int i = 0; i < numbers.get(1).size(); i++){
+            for (int i = 0; i < numbers.get(1).size(); i++) {
                 fNumbers[1] = fNumbers[1] * numbers.get(1).get(i);
             }
             return fNumbers;
         }
     }
-    
-    public static Map twoPoints(int x1, int y1, int x2, int y2){
+
+    public static Map twoPoints(int x1, int y1, int x2, int y2) {
         HashMap<Object, Object> exports = new HashMap<Object, Object>();
-       
+
+        // Finds midpoints
+        exports.put("midpoint_x", (x1 + x2) / 2);
+        exports.put("midpoint_y", (y1 + y2) / 2);
+
+        // Finds distance between the points
+        exports.put("distance", Math.sqrt(Math.pow((y2 - y1), 2) + Math.pow((x2 - x1), 2)));
+        exports.put("exact", Math.pow((y2 - y1), 2) + Math.pow((x2 - x1), 2));
+
+        // Finds the slope of the points
+        double slope = (y2 - y1) / (x2 - x1);
+        exports.put("slope", slope);
+
+        // Finds the base
+        double b = y1 - (slope * x1);
+
+        // Finds the line equation of the lines
+        exports.put("equation", slope + "x + " + b);
+
+        return exports;
+    }
+
+    public static Map onePoint(int x1, int y1, double slope) {
+        HashMap<Object, Object> exports = new HashMap<Object, Object>();
+
+        // Finds the slope of the points
+        exports.put("slope", slope);
+
+        // Finds the base
+        double b = y1 - (slope * x1);
+
+        // Finds the line equation of the lines
+        exports.put("equation", slope + "x + " + b);
+
         return exports;
     }
 }
